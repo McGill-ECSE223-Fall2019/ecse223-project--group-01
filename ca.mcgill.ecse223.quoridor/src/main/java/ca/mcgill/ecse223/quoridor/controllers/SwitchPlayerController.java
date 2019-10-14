508e287efe49258b7a31e8cdcc1cefe69a57b0cd
Tri-tin Truong <mark.zhu@mail.mcgill.ca>
2019-10-14 10:32:46 +0900

    Semi-merged into dev

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/SwitchPlayerController.java
@@ -0,0 +1,23 @@
+/**
+ * @author Mark Zhu
+ *
+ */
+
+package ca.mcgill.ecse223.quoridor.controllers;
+
+import ca.mcgill.ecse223.quoridor.model.Player;
+
+
+public class SwitchPlayerController {
+	
+	/**
+	 * This method switches the player that is currently active 
+	 * @param currentPlayerColor The color of the player that starts as active
+	 * @return The color player that ends as active
+	 */
+	
+	public static String SwitchActivePlayer(String currentPlayerColor) {
+		throw new UnsupportedOperationException();
+	}
+	
+}
--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
@@ -12,6 +12,7 @@ import ca.mcgill.ecse223.quoridor.QuoridorApplication;
 import ca.mcgill.ecse223.quoridor.controllers.BoardController;
 import ca.mcgill.ecse223.quoridor.controllers.UserController;
 import ca.mcgill.ecse223.quoridor.controllers.StartNewGameController;
+import ca.mcgill.ecse223.quoridor.controllers.SwitchPlayerController;
 import ca.mcgill.ecse223.quoridor.controllers.PositionController;
 import ca.mcgill.ecse223.quoridor.model.Board;
 import ca.mcgill.ecse223.quoridor.model.Direction;
@@ -732,6 +733,71 @@ public class CucumberStepDefinitions {
 		//GUI TODO later
 		throw new PendingException();
 	}
+	
+	
+	//************************************
+	//Switch Player
+	//@author: Mark Zhu
+	
+	String originalPlayerColor;
+	String nextPlayerColor;
+	
+	@Given("The player to move is {string}")
+	public void thePlayerToMoveIs(String playerColor) {
+		originalPlayerColor = playerColor;
+	}
+	
+	//@author: Mark Zhu
+	@And("The clock of {string} is running")
+	public void currentClockRunning(String playerColor) {
+		//TODO: GUI step
+	}
+	
+	//@author: Mark Zhu
+	@And("The clock of {string} is stopped")
+	public void nextClockStopped(String playerColor) {
+		//TODO: GUI step
+	}
+	
+	//@author: Mark Zhu
+	@When("Player {string} completes his move")
+	public void playerMoveCompleted(String playerColor) {
+		try {
+			nextPlayerColor = SwitchPlayerController.SwitchActivePlayer(playerColor);
+		} catch (UnsupportedOperationException e) {
+			throw new PendingException();
+		}
+	}
+	
+	//@author: Mark Zhu
+	@Then("The user interface shall be showing it is {string} turn")
+	public void displayWhoseTurn(String playerColor) {
+		//TODO: GUI step
+	}
+	
+	//@author: Mark Zhu
+	@And("The clock of {string} shall be stopped")
+	public void selfClockStop(String playerColor) {
+		//TODO: GUI step
+	}
+	
+	//@author: Mark Zhu
+	@And("The clock of {string} shall be running")
+	public void nextClockRun(String playerColor) {
+		//TODO: GUI step
+	}
+	
+	//@author: Mark Zhu
+	@And("The next player to move shall be {string}")
+	public void checkActivePlayer(String playerColor) {
+		if (originalPlayerColor == "white") {
+			assertEquals(nextPlayerColor,"black");
+		} else {
+			assertEquals(nextPlayerColor,"white");
+		}
+	}
+	//*****************************************************************
+	
 
 
 	// ***********************************************
