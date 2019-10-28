34bd5e35b44cfab86c40c1e7cea820c18236d623
Tempeus <nekugm@gmail.com>
2019-10-28 18:20:16 -0400

    SavePosition Controller updated

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/PositionController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/PositionController.java
@@ -1,6 +1,10 @@
 package ca.mcgill.ecse223.quoridor.controllers;
 
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
 import ca.mcgill.ecse223.quoridor.model.*;
+
+import java.io.*;
+import java.util.List;
 
 /**
  * This class is responsible for holding controller methods that will be used
@@ -13,6 +17,8 @@ public class PositionController {
      */
     public PositionController(){}
 
+    static String saveLocation = ".\\src\\main\\resources\\";
+
     /**
      * Attempts to create or overwrite a savefile,
      * that will contain the positions of the current game, into a filesystem.
@@ -23,8 +29,40 @@ public class PositionController {
      * @throws java.lang.UnsupportedOperationException
      */
     public static boolean saveGame(String filename, GamePosition position) throws java.lang.UnsupportedOperationException {
-        throw new java.lang.UnsupportedOperationException();
-    }
+        File file = new File(saveLocation + filename);
+        PrintWriter output;
+        if (file.exists() && !file.isDirectory()){ //If the save file exists and is not a directory
+            try {
+                output = new PrintWriter(new FileOutputStream(new File(saveLocation + filename), true));
+            } catch (FileNotFoundException e) { //Error check for writer
+                e.printStackTrace();
+                return false;
+            }
+        }
+        else {
+            try {
+                output = new PrintWriter(saveLocation + filename);
+            } catch (FileNotFoundException e) { //Error check for writer
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        //.Get the list of moves
+        List<Move> listOfMoves = QuoridorApplication.getQuoridor().getCurrentGame().getMoves();
+
+        //.*Not going into for loop, because there is nothing in listOfMoves
+        for(int i = 0; i < listOfMoves.size(); i++){
+            Move move = listOfMoves.get(i);
+            int moveNumber = move.getRoundNumber();
+            move.getNextMove();
+            //.****Not finished yet
+            output.append("\n");
+
+        }
+        output.close();
+        return true;
+       }
 
     /**
      * Attempts to load a specified savefile in a filesystem.
@@ -48,3 +86,16 @@ public class PositionController {
         throw new java.lang.UnsupportedOperationException();
     }
 }
+
+
+//Able to create/append to a file
+
+//PlayerPosition, GamePosition, WallPosition?
+// format: (round#). (whitePosition/whiteWall) (blackPosition/blackWall)
+
+//Q: Do each move require a .getPosition?
+//Q: how to loop through each move and store them using formatting
+
+//Either append after each turn to the associated round#
+//Append using formatting after each round
+//  Must determine if player put wall or player moved
