0b2f345d5e5eaa81741b4ab17b5dc0d425497fd0
Tritin Truong <truongtritin98@gmail.com>
2019-11-01 17:08:48 -0400

    Removed  print statements

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/SwitchPlayerController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/SwitchPlayerController.java
@@ -1,9 +1,5 @@
 package ca.mcgill.ecse223.quoridor.controllers;
 
-import java.sql.Time;
-import java.time.Duration;
-import java.time.Instant;
-
 import ca.mcgill.ecse223.quoridor.QuoridorApplication;
 import ca.mcgill.ecse223.quoridor.model.Player;
 
@@ -13,7 +9,6 @@ public class SwitchPlayerController {
 	 * 
 	 * This method switches the active and passive players
 	 * @param currentPlayerColor The color of the player that starts as active
-	 * @param timeSpent: The amount of time that the player spent as active
 	 * @return A string representing the color of the player that ends as active
 	 */
 	
@@ -26,7 +21,6 @@ public class SwitchPlayerController {
 		if (currentPlayerColor.equals("white")) {
 			//ModelQuery.getWhitePlayer().setRemainingTime(new Time(ModelQuery.getWhitePlayer().getRemainingTime().getTime()-timeSpent.toMillis()));
 			QuoridorApplication.getQuoridor().getCurrentGame().getCurrentPosition().setPlayerToMove(blackPlayer);
-			System.out.println("koko" + ModelQuery.getPlayerToMove());
 			return "black";
 		} else if (currentPlayerColor.equals("black")) {
 			//ModelQuery.getBlackPlayer().setRemainingTime(new Time(ModelQuery.getBlackPlayer().getRemainingTime().getTime()-timeSpent.toMillis()));
