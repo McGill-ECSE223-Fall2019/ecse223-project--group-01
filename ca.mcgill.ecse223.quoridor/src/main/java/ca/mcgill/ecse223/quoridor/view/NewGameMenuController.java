97961dd5e7c3923cbeaea8f41cd2f5b7759a28f5
FulinHuang <fulin.huang@mail.mcgill.ca>
2019-11-01 16:19:23 -0400

    add white player name

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/NewGameMenuController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/NewGameMenuController.java
@@ -1,8 +1,27 @@
 package ca.mcgill.ecse223.quoridor.view;
+import ca.mcgill.ecse223.quoridor.controllers.StartNewGameController;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
 
 public class NewGameMenuController extends ViewController{
 
+    @FXML
+    public TextField whitePlayerName;
+    public TextField blackPlayerName;
+    public TextField minutes;
+    public TextField seconds;
+    public void handleWhitePlayerName(ActionEvent actionEvent) {
+        try {
+
+        } catch (Exception e ) {
+
+        }
+
+        //LOAD NAME LIST IN LOAD GAME CONTROLLER
+    }
 
     public void backToMainMenu(ActionEvent actionEvent) {
         changePage("/fxml/Menu.fxml");
