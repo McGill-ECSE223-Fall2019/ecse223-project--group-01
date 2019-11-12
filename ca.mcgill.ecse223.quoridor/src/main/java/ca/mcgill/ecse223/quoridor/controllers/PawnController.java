6b04bdd42eebca3e7a2f55cbd800814bfe80e199
Tritin Truong <truongtritin98@gmail.com>
2019-11-12 12:14:35 -0500

    Setup for tests, initial controllers

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/PawnController.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse223.quoridor.controllers;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class PawnController {
+
+    public static  boolean movePawn(String side) throws NotImplementedException {
+        throw new NotImplementedException();
+    }
+
+    public static void getPossibleMoves() throws  NotImplementedException{
+        throw new NotImplementedException();
+    }
+}
