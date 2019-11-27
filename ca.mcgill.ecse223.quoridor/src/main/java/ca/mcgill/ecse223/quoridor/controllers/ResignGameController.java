c5014f3cb09c63186bf9fb9eeb8fec343a5f7239
jason <jasonlau13@gmail.com>
2019-11-26 20:47:48 -0500

    Added ResignGameController and changed the umple model

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controllers/ResignGameController.java
@@ -0,0 +1,18 @@
+package ca.mcgill.ecse223.quoridor.controllers;
+
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
+import ca.mcgill.ecse223.quoridor.model.Player;
+import ca.mcgill.ecse223.quoridor.model.Quoridor;
+
+public class ResignGameController {
+
+    public void setWinner(Player player) {
+
+        Quoridor quoridor = QuoridorApplication.getQuoridor();
+
+//        quoridor.getCurrentGame().getWhitePlayer();
+//        ModelQuery.getCurrentGame()
+
+
+    }
+}
