d533222ac78ef79d8b5202b2e09affaaf95b169e
jason <jasonlau13@gmail.com>
2019-11-30 13:35:01 -0500

    Basic action listeners done

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/InitializeBoardController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/InitializeBoardController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -30,6 +31,8 @@ import java.util.List;
 
 
 public class InitializeBoardController extends ViewController{
+
+
 
     enum PlayerState {WALL, PAWN, IDLE};
     PlayerState state = PlayerState.IDLE;
@@ -53,14 +56,95 @@ public class InitializeBoardController extends ViewController{
     public static boolean blackWon = false;
     public static boolean enableMovePawn = true;
     public static boolean enableDropWall = true;
+    public  PlayerPosition playerPosition ;
     public Rectangle rect1;
     public Rectangle rect2;
     public Rectangle rect3;
     public Rectangle rect4;
+    public Rectangle rect5;
+    public Rectangle rect6;
+    public Rectangle rect7;
+    public Rectangle rect8;
+    public Rectangle rect9;
+    public Rectangle rect10;
+    public Rectangle rect11;
+    public Rectangle rect12;
+    public Rectangle rect13;
+    public Rectangle rect14;
+    public Rectangle rect15;
+    public Rectangle rect16;
+    public Rectangle rect17;
+    public Rectangle rect18;
+    public Rectangle rect19;
+    public Rectangle rect20;
+    public Rectangle rect21;
+    public Rectangle rect22;
+    public Rectangle rect23;
+    public Rectangle rect24;
+    public Rectangle rect25;
+    public Rectangle rect26;
+    public Rectangle rect27;
+    public Rectangle rect28;;
+    public Rectangle rect29;
+    public Rectangle rect30;
+    public Rectangle rect31;
+    public Rectangle rect32;
+    public Rectangle rect33;
+    public Rectangle rect34;
+    public Rectangle rect35;
+    public Rectangle rect36;
+    public Rectangle rect37;
+    public Rectangle rect38;
+    public Rectangle rect39;
+    public Rectangle rect40;
+    public Rectangle rect41;
+    public Rectangle rect42;
+    public Rectangle rect43;
+    public Rectangle rect44;
+    public Rectangle rect45;
+    public Rectangle rect46;
+    public Rectangle rect47;
+    public Rectangle rect48;
+    public Rectangle rect49;
+    public Rectangle rect50;
+    public Rectangle rect51;
+    public Rectangle rect52;
+    public Rectangle rect53;
+    public Rectangle rect54;
+    public Rectangle rect55;
+    public Rectangle rect56;
+    public Rectangle rect57;
+    public Rectangle rect58;
+    public Rectangle rect59;
+    public Rectangle rect60;
+    public Rectangle rect61;
+    public Rectangle rect62;
+    public Rectangle rect63;
+    public Rectangle rect64;
+    public Rectangle rect65;
+    public Rectangle rect66;
+    public Rectangle rect67;
+    public Rectangle rect68;
+    public Rectangle rect69;
+    public Rectangle rect70;
+    public Rectangle rect71;
+    public Rectangle rect72;
+    public Rectangle rect73;
+    public Rectangle rect74;
+    public Rectangle rect75;
+    public Rectangle rect76;
+    public Rectangle rect77;
+    public Rectangle rect78;
+    public Rectangle rect79;
+    public Rectangle rect80;
+    public Rectangle rect81;
+
+
     public Circle testCircle;
 
 
     public void initialize() {
+
 
         //display player name
         whitePlayerName.setText(ModelQuery.getWhitePlayer().getUser().getName());
@@ -80,7 +164,76 @@ public class InitializeBoardController extends ViewController{
     	timerForBlackPlayer.setText(initialTime);
 
         state = PlayerState.IDLE;
+
         switchTimer();
+    }
+    public void handle(MouseEvent mouseEvent) {
+        Rectangle rec = (Rectangle)mouseEvent.getSource();
+        String z = rec.getId();
+        Double x = rec.getLayoutX();
+        Double y = rec.getLayoutY();
+        playerPosition = ModelQuery.getPlayerPositionOfPlayerToMove();
+
+        System.out.println("ID : "+z+" X: "+x+" Y: "+y);
+        if(checkPawnPostition(x,y, playerPosition).equals("up")){
+            PawnController.movePawn("up");
+        }
+
+
+
+                refresh();
+
+    }
+    public String checkPawnPostition(Double x, Double y, PlayerPosition playerPosition){
+        Tile tile = null;
+        String direction= null;
+
+        if(playerPosition.getPlayer().equals(ModelQuery.getWhitePlayer())){
+            tile = playerPosition.getTile();
+        }
+        else if (playerPosition.getPlayer().equals(ModelQuery.getBlackPlayer())){
+           tile = playerPosition.getTile();
+        }
+
+        Pair<Integer,Integer> coord = convertPawnToCanvas(tile.getRow(),tile.getColumn());
+
+        Double pawnX = (double)coord.getValue();
+        Double pawnY = (double)coord.getKey();
+
+        if (state==PlayerState.PAWN){
+            /*For handling pawn move*/
+
+                if (x-pawnX == -17 && y -pawnY == -60) {
+                    direction = "up";
+                }
+
+//                else if (code.equals(KeyCode.K)) {
+//                    PawnController.movePawn("down");
+//                }
+//                else if (code.equals(KeyCode.J)) {
+//                    PawnController.movePawn("left");
+//                }
+//                else if (code.equals(KeyCode.L)) {
+//                    PawnController.movePawn("right");
+//                }
+//                else if (code.equals(KeyCode.U)) {
+//                    PawnController.movePawn("upleft");
+//                }
+//                else if (code.equals(KeyCode.O)) {
+//                    PawnController.movePawn("upright");
+//                }
+//                else if (code.equals(KeyCode.N)) {
+//                    PawnController.movePawn("downleft");
+//                }
+//                else if (code.equals(KeyCode.COMMA)) {
+//                    PawnController.movePawn("downright");
+//                }
+
+        }
+        System.out.println("PawnX : "+pawnX+ " PawnY: "+pawnY);
+        System.out.println("Direction = "+direction);
+        return  direction;
+
     }
 
     public void switchTimer() {
@@ -181,6 +334,90 @@ public class InitializeBoardController extends ViewController{
 
         board.getChildren().add(rect1);
         board.getChildren().add(rect2);
+        board.getChildren().add(rect3);
+        board.getChildren().add(rect4);
+        board.getChildren().add(rect5);
+        board.getChildren().add(rect6);
+        board.getChildren().add(rect7);
+        board.getChildren().add(rect8);
+        board.getChildren().add(rect9);
+        board.getChildren().add(rect10);
+        board.getChildren().add(rect11);
+        board.getChildren().add(rect12);
+        board.getChildren().add(rect13);
+        board.getChildren().add(rect14);
+        board.getChildren().add(rect15);
+        board.getChildren().add(rect16);
+        board.getChildren().add(rect17);
+        board.getChildren().add(rect18);
+        board.getChildren().add(rect19);
+        board.getChildren().add(rect20);
+        board.getChildren().add(rect21);
+        board.getChildren().add(rect22);
+        board.getChildren().add(rect23);
+        board.getChildren().add(rect24);
+        board.getChildren().add(rect25);
+        board.getChildren().add(rect26);
+        board.getChildren().add(rect27);
+        board.getChildren().add(rect28);
+        board.getChildren().add(rect29);
+        board.getChildren().add(rect30);
+        board.getChildren().add(rect31);
+        board.getChildren().add(rect32);
+        board.getChildren().add(rect33);
+        board.getChildren().add(rect34);
+        board.getChildren().add(rect35);
+        board.getChildren().add(rect36);
+        board.getChildren().add(rect37);
+        board.getChildren().add(rect38);
+        board.getChildren().add(rect39);
+        board.getChildren().add(rect40);
+        board.getChildren().add(rect41);
+        board.getChildren().add(rect42);
+        board.getChildren().add(rect43);
+        board.getChildren().add(rect44);
+        board.getChildren().add(rect45);
+        board.getChildren().add(rect46);
+        board.getChildren().add(rect47);
+        board.getChildren().add(rect48);
+        board.getChildren().add(rect49);
+        board.getChildren().add(rect50);
+        board.getChildren().add(rect51);
+        board.getChildren().add(rect52);
+        board.getChildren().add(rect53);
+        board.getChildren().add(rect54);
+        board.getChildren().add(rect55);
+        board.getChildren().add(rect56);
+        board.getChildren().add(rect57);
+        board.getChildren().add(rect58);
+        board.getChildren().add(rect59);
+        board.getChildren().add(rect60);
+        board.getChildren().add(rect61);
+        board.getChildren().add(rect62);
+        board.getChildren().add(rect63);
+        board.getChildren().add(rect64);
+        board.getChildren().add(rect65);
+        board.getChildren().add(rect66);
+        board.getChildren().add(rect67);
+        board.getChildren().add(rect68);
+        board.getChildren().add(rect69);
+        board.getChildren().add(rect70);
+        board.getChildren().add(rect71);
+        board.getChildren().add(rect72);
+        board.getChildren().add(rect73);
+        board.getChildren().add(rect74);
+        board.getChildren().add(rect75);
+        board.getChildren().add(rect76);
+        board.getChildren().add(rect77);
+        board.getChildren().add(rect78);
+        board.getChildren().add(rect79);
+        board.getChildren().add(rect80);
+        board.getChildren().add(rect81);
+
+
+
+
+
 
 
 
